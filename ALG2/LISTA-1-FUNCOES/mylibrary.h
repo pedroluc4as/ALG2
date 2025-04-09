4d840a1645b71db3efcb266a43a68459d3326644
Pedro Sperandio <126828311+pedroluc4as@users.noreply.github.com>
2025-04-08 23:00:23 -0300

    Add files via upload

---
+++ ALG2/LISTA-1-FUNCOES/mylibrary.h
@@ -0,0 +1,28 @@
+//soma dois valores
+int soma(int num1, int num2){
+  int soma = 0;
+
+  soma = num1+num2;
+
+return soma;
+}
+//subtração dois valores
+int subtracao(int num1, int num2){
+  int subtracao = 0;
+  
+  subtracao = num1-num2;
+
+return subtracao;
+}
+//hello world!
+void helloworld()
+{
+  printf("Hello World\n");
+}
+//printf
+char printmsg(char msg)
+{
+  printf();
+  
+  return printf;
+}
